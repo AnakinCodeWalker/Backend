4e7ca114750ac28f1edc1b7d7e48de7503753a19
AnakinCodeWalker <parag3111ja@gmail.com>
2025-08-31 01:34:45 +0530

    more on node internals

---
+++ Node-Internals/day-2/flowOFCode.js
@@ -0,0 +1,21 @@
+//checkout the blog 
+https://nodejs.org/en/learn/asynchronous-work/event-loop-timers-and-nexttick
+
+//node js is a runtime enviorment 
+/*
+
+{ A single Threaded 
+js run time enviorment and  [c++ embedded with v8 Engine]
+with libuv - multiplatform c library for async input output 
+and thread pool
+}
+
+{
+
+}
+
+*/
+// a function which takes two args :- callback and time in milliseconds 
+// a async code which will execute after 2 seconds
+setTimeout(()=>{console.log("This is a setTimeOutfunction")},2000);
+
